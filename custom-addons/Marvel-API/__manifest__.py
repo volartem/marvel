d2c09c434be6660ca0b3427fa8b5b6378e12d01d
Artem <artem.volshynskyi@com>
2017-07-14 00:37:48 +0300

    create module Marvel-API

---
+++ custom-addons/Marvel-API/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Comics Marvel API',
+    'description': 'See comics from Marvel',
+    'author': 'VolArtem',
+    'depends': ['base'],
+    'application': True,
+    'summary': 'Module that allow to view comics from Marvel',
+    'version': ' 1.0.',
+    'license':  'LGPL-3',
+    'category': 'API',
+}
