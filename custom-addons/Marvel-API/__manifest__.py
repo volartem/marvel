c572fec263850e4b433c1fb54a4a2e5ba8812899
Artem <artem.volshynskyi@com>
2017-07-17 11:18:10 +0300

    depends from website and small css bootstrap changes

--- custom-addons/Marvel-API/__manifest__.py
+++ custom-addons/Marvel-API/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Comics Marvel API',
     'description': 'See comics from Marvel',
     'author': 'VolArtem',
-    'depends': ['base'],
+    'depends': ['website'],
     'application': True,
     'summary': 'Module that allow to view comics from Marvel',
     'version': ' 1.0.',
